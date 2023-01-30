24aedb576f4fc2b9b4b6db8e288141941f45128e
nithishreddy27 <nithishreddy0627@gmail.com>
2023-01-30 19:47:18 +0530

    nr added

--- pages/api/addEducation.js
+++ pages/api/addEducation.js
@@ -1,11 +1,11 @@
-import { addDetails } from "../../lib/user";
+import { AddDetails } from "../../lib/user";
 
 
 
 export default async function handler(req,res){
     console.log("inside add api ",req.body);
     try{
-        var data=await addDetails(req.body)
+        var data=await AddDetails(req.body)
         res.status(200).send({"done":true})
     } 
   catch (error) {
--- pages/api/addProfile.js
+++ pages/api/addProfile.js
@@ -1,4 +1,4 @@
-import { addDetails } from "../../lib/user";
+import { AddDetails } from "../../lib/user";
 
 
 
@@ -6,7 +6,7 @@ export default async function handler(req,res){
   
     console.log("inside add api ",req.body);
     try{
-        var data=await addDetails(req.body)
+        var data=await AddDetails(req.body)
         res.writeHead(302, { Location: '/student/profile' })
         res.end()
     } 
